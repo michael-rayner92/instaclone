b4988d81fe0fd72297a7a97b9c9d9b225bb6e376
Michael Rayner <scetils_michael@hotmail.com>
2021-09-19 19:32:13 +1000

    Added Header and Started Populating Dashboard
    User context and hooks added for auth checks

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,8 @@
 import { firebase } from '../lib/firebase';
 
+/**
+ * Check firestore is username is already used
+ */
 export async function doesUsernameExist(username) {
   const result = await firebase
     .firestore()
@@ -8,4 +11,18 @@ export async function doesUsernameExist(username) {
     .get();
 
   return !!result.docs.map(user => user.data().length > 0).length;
+}
+
+/**
+ * Get user from the firestore where userId === userId (passed from the auth context)
+ */
+export async function getUserByUserId(userId) {
+  const result = await firebase.firestore().collection('users').where('userId', '==', userId).get();
+
+  const user = result.docs.map(item => ({
+    ...item.data(),
+    docId: item.id
+  }));
+
+  return user;
 }
