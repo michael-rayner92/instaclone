f2cf35b4c7623af35879710b46e5c6634efbd29a
Michael Rayner <scetils_michael@hotmail.com>
2021-09-26 20:07:25 +1000

    ci: :green_heart: Adjust Lighthouse Config

---
+++ lighthouserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  ci: {
+    upload: {
+      target: 'temporary-public-storage'
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'csp-xss': 'warn',
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }]
+      }
+    },
+    collect: {
+      url: ['http://127.0.0.1:4000'],
+      startServerCommand: 'http-server ./build -p 3000 -g'
+    }
+  }
+};
